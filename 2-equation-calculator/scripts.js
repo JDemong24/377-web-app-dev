5d2b0b6d5fecfc9b12f53b583c790c53b68ac680
Jacob DeMong <jdemong24@hanoverstudents.org>
2022-09-26 12:45:28 -0400

    stuff

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -1,43 +1,45 @@
+
+
 function velocity() {
-    var num1 = parseInt(document.getElementById("value1").value);
-    var num2 = parseInt(document.getElementById("value2").value);
+    var num1 = parseInt(document.getElementById("Distance1").value);
+    var num2 = parseInt(document.getElementById("Time1").value);
 
     var result = num1 / num2;
 
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("velocityResult").innerHTML = result;
 }
 
 function acceleration() {
-    var num1 = parseInt(document.getElementById("value1").value);
-    var num2 = parseInt(document.getElementById("value2").value);
+    var num1 = parseInt(document.getElementById("Length1").value);
+    var num2 = parseInt(document.getElementById("Time2").value);
 
     var result = num1 / (num2*num2);
 
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("accelerationResult").innerHTML = result;
 }
 
 function density() {
-    var num1 = parseInt(document.getElementById("value1").value);
-    var num2 = parseInt(document.getElementById("value2").value);
+    var num1 = parseInt(document.getElementById("Mass1").value);
+    var num2 = parseInt(document.getElementById("Length2").value);
 
     var result = num1/(num2*num2*num2);
 
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("densityResult").innerHTML = result;
 }
 function area() {
-    var num1 = parseInt(document.getElementById("value1").value);
-    var num2 = parseInt(document.getElementById("value2").value);
+    var num1 = parseInt(document.getElementById("Length3").value);
+    var num2 = parseInt(document.getElementById("Width1").value);
 
     var result = num1*num2;
 
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("areaResult").innerHTML = result;
 }
 function volume() {
-    var num1 = parseInt(document.getElementById("value1").value);
-    var num2 = parseInt(document.getElementById("value2").value);
-    var num3 = parseInt(document.getElementById("value3").value);
+    var num1 = parseInt(document.getElementById("Length4").value);
+    var num2 = parseInt(document.getElementById("Width2").value);
+    var num3 = parseInt(document.getElementById("Height1").value);
 
     var result = num1*num2*num3;
 
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("volumeResult").innerHTML = result;
 }
