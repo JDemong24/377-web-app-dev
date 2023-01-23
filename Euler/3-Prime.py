21eb57a6999a8970a7deb4e94cec38979706e59f
Jacob DeMong <jdemong24@hanoverstudents.org>
2023-01-23 12:17:07 -0500

    stuff 2

--- Euler/3-Prime.py
+++ Euler/3-Prime.py
@@ -1,12 +1,10 @@
 def pt3():
-    num=1
-    final=0
+    num1=600851475143
     num2=2
-    while num<=100:
-        while num2%num==0:
-            num=num/num2
+    while num2<num1:
+        while num1%num2==0:
+            num1=num1/num2
         num2=num2+1
-    num=num+1
-    print(final)
+    print(num1)
 
 pt3()
